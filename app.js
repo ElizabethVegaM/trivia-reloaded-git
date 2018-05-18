18a551a8e26a1f78f531775565ce711e64c6e140
Carolina Araya A <_carolina_@hotmail.cl>
2018-05-18 13:03:43 -0400

    puse lo tuyo en una funcion, pero aun no funciona

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 document.getElementById("enteredName").addEventListener("click", function(){
-    var username = document.getElementById("username").value;
-    document.getElementById("demo").innerHTML = "Bienvenida " + username;
+var username = document.getElementById("username").value;
+document.getElementById("demo").innerHTML = "Bienvenida " + username;
 
-function yesPlay() {
-    
-}
 
+function questions(){
 document.getElementById("yesPlay").addEventListener("click", yesPlay);
 function yesPlay() {
     document.getElementById("gameplay").style.display='none';
     document.getElementById('question1').innerHTML='<h3>¿Han egresado hombres en Laboratoria?</3>';
     document.getElementById("answer1").style.display='block';
 }
-
+}
 
 document.getElementById("noPlay").addEventListener("click", noPlay);
 function noPlay() {
